c2af4cd152c90547b6ae607973d41c26d322fb02
joenwoke <jnwoke@hawk.iit.edu>
2025-04-16 19:48:44 -0500

    Continued initializing variables for tipSlider and tipPercent

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const billTotal = document.getElementById("billTotal");
+	const tipSlider = document.getElementById("tipSlider");
+	const tipPercent = document.getElementById("tipPercent");
+	
 });
