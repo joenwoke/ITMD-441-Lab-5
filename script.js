35a0d24dabcb22772bb35da54c5b509602bb3d70
joenwoke <jnwoke@hawk.iit.edu>
2025-04-16 19:55:59 -0500

    Updated function to update Tip Amount, Total with tax and toal with tip + tax everytime the user types in a new value and moves the tip slider

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
 	const totalWithTipTax = document.getElementById("totalWithTipTax");
 	
 	function updateValues() {
+		let bill = parseFloat(billTotal.value);
+		let tip = parseInt(tipSlider.value);
 		
+		if (isNaN(bill) || bill < 0) {
+			tipAmount.value = '';
+			totalWithTax.value = '';
+			totalWithTipTax.value = '';
+			return;
+		}
+		
+		tipPercent.textContent = `${tip}%`;
+		
+		const tax = bill * 0.11;
+		const tipAmt = bill * (tip / 100);
+		const withTax = bill + tax;
+		const finalTotal = withTax + tipAmt;
+		
+		tipAmount.value = tipAmt.toFixed(2);
+		totalWithTax.value = withTax.toFixed(2);
+		totalWithTipTax.value = finalTotal.toFixed(2);
 	}
 });
