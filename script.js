2f064eecead705eb78a90ce0a493ef80047042a5
joenwoke <jnwoke@hawk.iit.edu>
2025-04-16 19:51:31 -0500

    Completed initialization of constant variables for calculations

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const billTotal = document.getElementById("billTotal");
 	const tipSlider = document.getElementById("tipSlider");
 	const tipPercent = document.getElementById("tipPercent");
-	
+	const tipAmount = document.getElementById("tipAmount");
+	const totalWithTax = document.getElementById("totalWithTax");
+	const totalWithTipTax = document.getElementById("totalWithTipTax");
 });
